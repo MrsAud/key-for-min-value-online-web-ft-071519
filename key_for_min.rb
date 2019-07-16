04b285131f447aa753b53e5dbd84850a34f99616
Audrea Cook <audreacwork@gmail.com>
2019-07-16 16:50:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   name_hash.each do |item, value|
     min = value
-    if min > value
+    if min >= value
       min == value
     else
       name_hash.delete(item)

6a6c8b5205f34e6f9bc07de82de6c65cc3acf961
Audrea Cook <audreacwork@gmail.com>
2019-07-16 16:48:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,10 @@ require 'pry'
 def key_for_min_value(name_hash)
   name_hash.each do |item, value|
     min = value
-    name_hash.delete_if
+    if min > value
+      min == value
+    else
+      
     
     binding.pry
     
